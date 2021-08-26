385b361bb4a8af188550e8ada836e84e755027f1
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-08-26 10:39:46 +0800

    Added solution file

--- src/CodeCompanion.Auditing/Footprint.cs
+++ src/CodeCompanion.Auditing/Footprint.cs
@@ -1,14 +1,59 @@
+using System;
+
 namespace CodeCompanion.Auditing
 {
-    public struct Footprint
+    public class Footprint : IFootprint
     {
-        public readonly string Name;
-        public readonly object Value;
-
-        public Footprint(string name, object value)
+        public object this[string name]
         {
-            Name = name;
-            Value = value;
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(string name, object value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool TryAdd(string name, object value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool TryGet(string name, out object value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool TryRemove(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool TryUpdate(string name, object value)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(string name, object value)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
