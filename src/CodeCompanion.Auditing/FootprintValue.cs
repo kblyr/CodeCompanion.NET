f4e59dc1a73dce8098f4062d31f0d57838508065
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-08-27 20:38:44 +0800

    FootprintValue.CacheExpiration Added field for expiration if the footprint value is cached

--- src/CodeCompanion.Auditing/FootprintValue.cs
+++ src/CodeCompanion.Auditing/FootprintValue.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace CodeCompanion.Auditing
 {
     public struct FootprintValue
@@ -6,13 +8,15 @@ namespace CodeCompanion.Auditing
         public readonly object Value;
         public readonly bool IsOneTime;
         public readonly bool IsCached;
+        public readonly TimeSpan? CacheExpiration;
 
-        public FootprintValue(string name, object value, bool isOneTime = false, bool isCached = false)
+        public FootprintValue(string name, object value, bool isOneTime = false, bool isCached = false, TimeSpan? cacheExpiration = null)
         {
             Name = name;
             Value = value;
             IsOneTime = isOneTime;
             IsCached = isCached;
+            CacheExpiration = cacheExpiration;
         }
 
         public static FootprintValue Create(string name, object value) => new(name, value);
