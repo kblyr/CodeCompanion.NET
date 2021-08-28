9c684bff0198457906b149e8d54763988675700a
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-08-28 22:50:09 +0800

    DependencyInjection

---
+++ src/DependencyInjection/IServiceCollectionExtensions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeCompanion
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static ICodeCompanionServiceCollection AddCodeCompanion(this IServiceCollection instance) => new CodeCompanionServiceCollection(instance);
+    }
+}
